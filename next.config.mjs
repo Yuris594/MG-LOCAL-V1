319acfa08ae9e6ccc5fb8e03f2d479235b89d589
Yuris594 <yurisbolanos594@gmail.com>
2024-08-30 15:39:30 -0500

    cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //  output: 'export',
-  };
+    // output: 'export',
+};
   
-  //export default nextConfig;
+ // export default nextConfig;
   
   
-  export default {
+ export default {
     async rewrites() {
       return [
         {
