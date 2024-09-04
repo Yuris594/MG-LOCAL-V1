2c6407d979fd7742bbb66cf1130172343c99ff48
Yuris594 <yurisbolanos594@gmail.com>
2024-09-04 07:07:01 -0500

    cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
   const nextConfig = {
-      output: 'export',
+      //output: 'export',
 
-      async headers() {
+      async rewrites() {
         return [
           {
             source: '/api/:path*',    
-            //destination: 'http://172.20.20.3:8001/:path*',        
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" }, // O puedes especificar un dominio
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ],
+            destination: 'http://172.20.20.3:8001/:path*',        
           }
         ]
       }
