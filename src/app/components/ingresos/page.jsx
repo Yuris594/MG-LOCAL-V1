53d5a816bfcb651ae933e8f73666456d50f43b7c
Yuris594 <yurisbolanos594@gmail.com>
2024-09-05 10:36:01 -0500

    cambios

--- src/app/components/ingresos/page.jsx
+++ src/app/components/ingresos/page.jsx
@@ -129,7 +129,7 @@ const Ingresos = () => {
 
     try {
       espera();
-      const { datos } = await Peticion(Global.url + "control_entradas/documento/" + cedula, "POST");
+      const { datos } = await Peticion("/api/control_entradas/documento/" + cedula, "POST");
 
       if (online === true) {
         if (res.ok) {
