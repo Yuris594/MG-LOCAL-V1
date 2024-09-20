cc97303c02ab86c4c0ac9cb57ea0b198d4aa7628
Yuris594 <yurisbolanos594@gmail.com>
2024-09-20 14:11:12 -0500

    Cambio

--- src/app/components/iniciar/page.jsx
+++ src/app/components/iniciar/page.jsx
@@ -1,4 +1,4 @@
-
+/*
 const Iniciar = async (usuario, clave) => {
     const response = await fetch(`http://172.20.20.3:8001/usuarios/listar/${usuario}/${clave}`, {
       method: "GET",
@@ -13,3 +13,17 @@ const Iniciar = async (usuario, clave) => {
 };
 
 export default Iniciar;
+*/
+
+import Inicio from '@/app/start/page'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Inicio />
+    </div>
+  )
+}
+
+export default page
