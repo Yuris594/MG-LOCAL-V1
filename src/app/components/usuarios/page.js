f3da5f47c1ade2d15757e6052f5314caf716e0ed
Yuris594 <yurisbolanos594@gmail.com>
2024-09-23 09:31:09 -0500

    Cambios..

--- src/app/components/usuarios/page.js
+++ src/app/components/usuarios/page.js
@@ -1,6 +1,5 @@
 import Usuarios from "@/app/start/usuarios/page";
 
-export const revalidate = 60
 
 const PageUsuario = async () => {
     const response = await fetch('http://172.20.20.3:8001/usuarios/listar');
